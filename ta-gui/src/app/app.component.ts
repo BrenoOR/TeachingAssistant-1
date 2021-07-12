99e58300f8a07c13c68febb571898fa64e83cee7
Breno Rodrigues <bor@cin.ufpe.br>
2021-07-11 22:23:20 -0300

    Servidor Integrado ao cliente

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -1,34 +1,11 @@
 import { Component } from '@angular/core';
 import { NgModule } from '@angular/core';
 
-import { Aluno } from '../../../common/aluno';
-import { AlunoService } from './aluno.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   constructor(private alunoService: AlunoService) {}
-
-   aluno: Aluno = new Aluno();
-   alunos: Aluno[] = [];
-   cpfduplicado: boolean = false;
-
-   criarAluno(a: Aluno): void {
-     if (this.alunoService.criar(a)) {
-       this.alunos.push(a);
-       this.aluno = new Aluno();
-       console.log("Aluno",a.nome, "cadastrado com sucesso!");
-     } else {
-       console.error("CPF", this.aluno.cpf, "já cadastrado!");
-       this.cpfduplicado = true;
-     }
-   }
-
-   onMove(): void {
-      this.cpfduplicado = false;
-   }
 
 }
