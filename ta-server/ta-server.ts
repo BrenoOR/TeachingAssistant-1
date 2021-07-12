65ffb7846c79fe4d2f69b8162701d015b6095868
Breno Rodrigues <bor@cin.ufpe.br>
2021-07-11 22:17:52 -0300

    Adaption to current project with github field

--- ta-server/ta-server.ts
+++ ta-server/ta-server.ts
@@ -8,30 +8,57 @@ var taserver = express();
 
 var alunos: CadastroDeAlunos = new CadastroDeAlunos();
 
+var msgType;
+
 taserver.use(bodyParser.json());
+
+taserver.get('', function (req, res){
+  var aluno: string = JSON.stringify(alunos.getAlunos());
+  msgType = "getAlunos";
+  res.send(aluno);
+  console.log(msgType, "message sent...");
+})
 
 taserver.get('/alunos', function (req, res) {
   var aluno: string = JSON.stringify(alunos.getAlunos());
+  msgType = "getAlunos";
   res.send(aluno);
+  console.log(msgType, "message sent...");
+})
+
+taserver.get('/aluno', function (req, res) {
+  var aluno: Aluno = <Aluno> req.body;
+  if (!alunos.cpfNaoCadastrado(aluno.cpf)) {
+    var resp: string = JSON.stringify(aluno);
+    res.send(resp);
+  } else {
+    res.send({"failure": "Aluno não encontrado"});
+  }
 })
 
 taserver.post('/aluno', function (req: express.Request, res: express.Response) {
+  msgType = "createAluno";
   var aluno: Aluno = <Aluno> req.body; //verificar se é mesmo Aluno!
   aluno = alunos.criar(aluno);
   if (aluno) {
     res.send({"success": "O aluno foi cadastrado com sucesso"});
+    console.log(msgType, "success message sent...");
   } else {
     res.send({"failure": "O aluno não pode ser cadastrado"});
+    console.error(msgType, "failure message send...");
   }
 })
 
 taserver.put('/aluno', function (req: express.Request, res: express.Response) {
+  msgType = "updateAluno";
   var aluno: Aluno = <Aluno> req.body;
   aluno = alunos.atualizar(aluno);
   if (aluno) {
     res.send({"success": "O aluno foi atualizado com sucesso"});
+    console.log(msgType, "success message sent...");
   } else {
     res.send({"failure": "O aluno não pode ser atualizado"});
+    console.error(msgType, "failure message send...");
   }
 })
 
